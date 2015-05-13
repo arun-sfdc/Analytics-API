1de32cfde253cd480e41805588b92b23d1fd8cc3
ArunV <arun_sfdc@yahoo.com>
2015-05-12 23:17:57 -0700

    Oauth support

--- MassEmail/src/index.js
+++ MassEmail/src/index.js
@@ -5,12 +5,18 @@ var Mustache = require('mustache');
 var jsforce = require('jsforce');
 var morgan = require('morgan');
 var _ = require('underscore');
+var compression = require('compression')
 
 // Constants
-var LOGIN_URL = 'https://salesforce.com';
 var PORT = 9000;
-var TEMPLATE = './src/index.mst';
+var TEMPLATE = 'index.mst';
+var TEMPLATE_TABLE = 'table.mst';
 var PAGESIZE = 2000;
+var OAUTH2 = new jsforce.OAuth2({
+		clientId : process.env.CID,
+		clientSecret : process.env.CS,
+		redirectUri : process.env.URL
+});
 
 // PATCH JSFORCE for Turbo Pilot
 var EXECUTE_ASYNC = function(options, callback) {
@@ -46,52 +52,68 @@ var RETRIEVE_PAGE = function(instance, mylastRowIdFetched, callback) {
 // App
 var app = express();
 app.use(morgan());
+app.use(compression());
+app.get('/auth', function(req, res) {
+		res.redirect(OAUTH2.getAuthorizationUrl({ scope : 'full' }));
+});
 app.get('/', function (req, res) {
-				console.log(new Date());
-				var username = req.query.un;
-				var password = req.query.pw;
-				var reportid = req.query.id;
-				var conn = new jsforce.Connection({loginUrl: LOGIN_URL});
-				conn.login(username, password, function(err, resp) {
+		var code = req.query.code;
+		if(!code) {
+			res.redirect('/auth');
+		}
+		var conn = new jsforce.Connection({ oauth2: OAUTH2 });
+  		conn.authorize(code, function(err, userInfo) {
+				conn.analytics.reports(function(err, reports) {
 						if (err) { return console.error(err); }
-						conn.analytics.reports(function(err, reports) {
+						var reportId = reports[0].id;
+						fs.readFile(TEMPLATE, function (err, data) {
+								res.write(Mustache.render(data.toString(), {reportid: reportId, refreshToken: conn.refreshToken, accessToken: conn.accessToken, instanceUrl: conn.instanceUrl}));
+								res.end();
+						});
+				});
+		});
+}); 
+app.get('/report', function (req, res) {
+		console.log(new Date());
+		var conn = new jsforce.Connection({
+				oauth2 : OAUTH2,
+				instanceUrl : req.query.instanceUrl,
+				accessToken : req.query.accessToken,
+				refreshToken : req.query.refreshToken
+		});
+		var report = conn.analytics.report(req.query.reportId);
+		report.executeAsync = EXECUTE_ASYNC;
+		report.executeAsync({turbo: true}, function(err, instance) {
+				if (err) { return console.error(err); }
+				var pollToComplete = function() {
+						var myinstance = report.instance(instance.id);
+						myinstance.retrieve(function(err, result) {
 								if (err) { return console.error(err); }
-								var reportId = reportid||reports[0].id; 
-								var report = conn.analytics.report(reportId);
-								report.executeAsync = EXECUTE_ASYNC;
-								report.executeAsync({turbo: true}, function(err, instance) {
-										if (err) { return console.error(err); }
-										var pollToComplete = function() {
-												var myinstance = report.instance(instance.id);
-												myinstance.retrieve(function(err, result) {
+								if(result.attributes.status == 'Success') {
+										var rows = [];
+										myinstance.retrievePage = RETRIEVE_PAGE;
+										var getNextPage = function(lastRow) {
+												myinstance.retrievePage(myinstance, lastRow, function(err, pageresult) {
 														if (err) { return console.error(err); }
-														if(result.attributes.status == 'Success') {
-																var rows = [];
-																myinstance.retrievePage = RETRIEVE_PAGE;
-																var getNextPage = function(lastRow) {
-																		myinstance.retrievePage(myinstance, lastRow, function(err, pageresult) {
-																				if (err) { return console.error(err); }
-																				rows.push(pageresult.factMap['T!T'].rows);
-																				if(lastRow >= 100000) {
-																						fs.readFile(TEMPLATE, function (err, data) {
-																								res.write(Mustache.render(data.toString(), {data: rows}));
-																								res.end();
-																						});
-																						console.log(new Date());
-																				} else {
-																						getNextPage(lastRow+PAGESIZE);
-																				}
-																		});
-																};
-																getNextPage(0);		
+														rows.push(pageresult.factMap['T!T'].rows);
+														if(lastRow >= 100000) {
+																fs.readFile(TEMPLATE_TABLE, function (err, data) {
+																		res.write(Mustache.render(data.toString(), {data: rows}));
+																		res.end();
+																});
+																console.log(new Date());
 														} else {
-																pollToComplete();
+																getNextPage(lastRow+PAGESIZE);
 														}
 												});
 										};
+										getNextPage(0);		
+								} else {
 										pollToComplete();
-								});
+								}
 						});
-				});
+				};
+				pollToComplete();
+		});
 });
 app.listen(PORT);
